f5cf4d0643fd4faaecd7736330fceb317483f8f2
lycho33 <cho.lyds@gmail.com>
2022-01-31 17:44:19 -0500

    added icons and text to the Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { DiGithubBadge } from "react-icons/di";
+import { FaDev } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import './css/Footer.css'
 
 function Footer() {
   return <div>
-      <h2>Footer</h2>
+      <div className='contact info--flexbox'>
+        <h2>CONTACT</h2>
+        <a href="https://github.com/lycho33" className='github'><h1><DiGithubBadge size={42}/> </h1></a>
+        <a href="https://dev.to/lycho33" className='dev'><h1><FaDev size={42}/></h1></a>
+        {/* <a href="mailto:lydiacho1@outlook.com" className='email'><h1><IoIosMail size={52}/></h1></a> */}
+      </div>
   </div>;
 }
 
