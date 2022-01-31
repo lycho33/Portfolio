2d72069dde678ada143a75e16991061b79c994f8
lycho33 <cho.lyds@gmail.com>
2022-01-31 18:00:20 -0500

    added a line on the top of footer componenet

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,14 @@ import { FaDev } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import './css/Footer.css'
 
+const ColoredLine = ({ color }) => (
+    <hr style={{color, backgroundColor: color, height: 1.5}} />
+);
+
 function Footer() {
   return <div>
+      {/* <ColoredLine color="#6eff3c" /> */}
+      <hr />
       <div className='contact info--flexbox'>
         <h2>CONTACT</h2>
         <a href="https://github.com/lycho33" className='github'><h1><DiGithubBadge size={42}/> </h1></a>
