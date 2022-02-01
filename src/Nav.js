957333da15a5775f0f4219e0b77649965e08f596
lycho33 <cho.lyds@gmail.com>
2022-02-01 14:09:49 -0500

    handle function when clicking the button

--- src/Nav.js
+++ src/Nav.js
@@ -6,10 +6,13 @@ function Nav() {
     
     const [navbarOpen, setNavbarOpen] = useState(false)
     
+    const handleToggle = () => {
+        setNavbarOpen(!navbarOpen)
+    }
   return <nav>
       <h3>Nav Bar</h3>
-      <button>{navbarOpen ? "Close" : "Open"}</button>
-      <ul></ul>
+      <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
+      <ul className={`menuNav ${navbarOpen ? "showMenu" : ""}`}></ul>
   </nav>;
 }
 
