5cd1a5ea6e4f4406a6fd8f971f0716dc498dd6f0
lycho33 <cho.lyds@gmail.com>
2022-02-01 17:04:56 -0500

    added a vertical line with styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../css/Home.css'
 
 function Home() {
   return <div>
-      <h1>TESTING</h1>
+      <div className='vertical-line'>
+      </div>
+      <div>
+        <h1>hello</h1>
+      </div>
   </div>;
 }
 
