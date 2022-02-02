edcfabd984c8a660ada335648a87f4a366b64d63
lycho33 <cho.lyds@gmail.com>
2022-02-02 16:21:22 -0500

    added a link for the initials icon

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react"
+import { Link } from 'react-router-dom'
 import { VscMenu } from "react-icons/vsc";
 import './css/Header.css'
 
 function Header({showNav, setShowNav}) {
+
   return <header>
-      <div className="circle">
-          <p className="text">LC</p>
-      </div>
+      <Link to="/">
+        <div className="circle">
+            <p className="text">LC</p>
+        </div>
+      </Link>
+
       <div className='icon hamburger'>
         <VscMenu size={40} onClick={() => setShowNav(!showNav)}/>
       </div>
