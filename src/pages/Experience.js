d6518fa3b3616c672fb3ab40a3994f5ae06f7030
lycho33 <cho.lyds@gmail.com>
2022-02-04 18:53:57 -0500

    table of experience info

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import '../css/Experience.css'
+import {ExperienceData} from './ExperienceData'
 
 function Experience({child}) {
+  const handleButtons = () => {
+    
+  }
+
   return <div style={child} className='child bg2'>
-      Experience
+      <div className='experience-contents'>
+        <h1>Experience</h1>
+        {ExperienceData.map((info, index) => {
+          return (
+          <table className='resume'>
+            <tr>
+              <th className='company'><button>{info.company}</button></th>
+              <th className='description'>
+                <h3>{info.jobTitle}</h3>
+                <li>{info.description}</li>
+              </th>
+            </tr>
+          </table>
+          )
+        }) }
+      </div>
   </div>;
 }
 
