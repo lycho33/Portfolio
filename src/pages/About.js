02a480cde91b6afef1ea46480fe972d8cc60ae13
lycho33 <cho.lyds@gmail.com>
2022-02-04 14:17:37 -0500

    more styling for About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import HorizontalScroll from 'react-scroll-horizontal';
+import ProfilePic from '../image/profilePic.jpg'
+import '../css/About.css'
 
 
 function About({child}) {
   return <div style={child} className='child bg1'>
-      <h1>About</h1>
+      <div className='contents'>
+        <div className='bio'>
+          <h1 className='about-title'>About Me</h1>
+          <p>Software Engineer with experience in Ruby on Rails, JavaScript and React and a background as both an educator and a professional violinist. With a love of problem solving, how others experience something and solving technical problems, I have the tenacity and flexibility to adjust to others in order to accomplish a project. </p>
+          <h2>Technical Skills</h2>
+          <ul className='languages'>
+            <li>React</li>
+            <li>Redux</li>
+            <li>JavaScript</li>
+            <li>Ruby</li>
+          </ul>
+        </div>
+        <img src={ProfilePic} alt="Profile Pic" />
+      
+      </div>      
   </div>;
 }
 
