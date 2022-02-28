0d5d5e8b8a20cfca88750ade39af4d8e68b2683a
lycho33 <cho.lyds@gmail.com>
2022-02-27 20:58:23 -0500

    styled About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,10 +5,8 @@ import '../css/About.css'
 import {motion} from 'framer-motion'
 import { Link } from 'react-router-dom'
 import { FaReact } from 'react-icons/fa';
-import { SiRedux } from 'react-icons/si';
-import { SiJavascript } from 'react-icons/si';
+import { SiRedux, SiSqlite, SiJavascript, SiRubyonrails  } from 'react-icons/si';
 import { DiRuby } from 'react-icons/di';
-import { SiRubyonrails } from 'react-icons/si';
 import { AiFillGithub } from 'react-icons/ai';
 
 const containerVariants = {
@@ -43,13 +41,16 @@ function About() {
               <p>Software Engineer with experience in Ruby on Rails, JavaScript and React and a background as both an educator and a professional violinist. With a love of problem solving, how others experience something and solving technical problems, I have the tenacity and flexibility to adjust to others in order to accomplish a project. </p>
               <h2>Technical Skills</h2>
               <ul className='languages'>
-                <li><FaReact color="#02FFF0"/> React</li>
-                <li><SiRedux color="#8A2BE2"/> Redux</li>
-                <li><SiJavascript color="yellow"/> JavaScript</li>
-                <li><DiRuby color="red"/> Ruby</li>
-                <li><SiRubyonrails color='red' size={20}/> Ruby on Rails</li>
-                <li><AiFillGithub color='black' size={20}/> Github</li>
-                <li>Sqlite3</li>
+                <li><FaReact size={30}color="#02FFF0"/> React</li>
+                <li><SiRedux size={30}color="#8A2BE2"/> Redux</li>
+                <li><SiJavascript size={30}color="yellow"/> JavaScript</li>
+                <li><DiRuby size={30}color="red"/> Ruby</li>
+     
+              </ul>
+              <ul className='languages'>
+                <li><SiRubyonrails color='red' size={30}/> Ruby on Rails</li>
+                <li><AiFillGithub color='black' size={30}/> Github</li>
+                <li><SiSqlite color='blue' size={30}/>Sqlite3</li>
               </ul>
             </div>
             <img src={ProfilePic} alt="Profile Pic" />
