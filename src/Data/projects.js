570a8fb485197e242a3c02a981f08ed74dfbbad9
lycho33 <cho.lyds@gmail.com>
2022-03-14 17:26:08 -0400

    edited about page

---
+++ src/Data/projects.js
@@ -0,0 +1,16 @@
+export const allProjects = [
+    {
+        name: "BuddyUp",
+        image: "",
+        description: "This is a chat engine app where you can pair up to message a friend.",
+        repo: "https://github.com/lycho33/BuddyUp",
+        demo: "https://youtu.be/vFqZduiq3TM"
+    },
+    {
+        name: "Teachers Unite",
+        image: "",
+        description: "This app is a mini-eCommerce site where you can add items to cart and purchase added items",
+        repo: "https://github.com/lycho33/vocab-ecommerce-frontend",
+        demo: "https://youtu.be/vFqZduiq3TM"
+    }
+]
--- src/pages/About.js
+++ src/pages/About.js
@@ -38,7 +38,11 @@ function About() {
           <div className='contents'>
             <div className='bio'>
               <h1 className='about-title'>About Me</h1>
-              <p className='font-link'>Software Engineer with experience in Ruby on Rails, JavaScript and React and a background as both an educator and a professional violinist. With a love of problem solving, how others experience something and solving technical problems, I have the tenacity and flexibility to adjust to others in order to accomplish a project. </p>
+              <p>
+                <span className='summary'>I have always been a musician and an educator and have always fueled my passion with the adventure of building lifetime skills. As a musician, I was driven to hone my craft to the level of the great masters and dreamed to achieve a level where I was released from the worries of techniques and free to achieve maximum artistic expression. As an educator, my mission was to help students develop the right tools to gain ownership and intrinsic motivation over their learners-journey. Asking carefully scaffolded questions, learning to problem-solve on one's own, understanding one's learning process, and repeatedly reviewing what one's goals are, were all components I believed essential tools.</span>
+                <br/><br/><span className='summary'>From years of this pursuit, I realized that my insatiable desire to build and teach skills did not stop with music. I wanted to learn how to build apps. After a month of joining the Flatiron School, I quickly learned that the problem-solving I so enjoyed in my other fields were just as fun when I was coding. Not only could I enjoy the journey of my problem-solving skills improve, the infinite possibilities of making an imagination a reality gave me the same thrill I had felt from music.</span>
+                <br/><br/><span className='summary'>Here I am after Flatiron as a full-stack engineer. I have never been more excited to continue my growth. The lessons I learned as a musician/educator have evolved due to programming and I can't wait to see how far I can go.</span>
+              </p>
               <h2>Technical Skills</h2>
               <ul className='languages'>
                 <li><FaReact size={30}color="#02FFF0"/><span className='lang-name'> React</span></li>
@@ -46,8 +50,6 @@ function About() {
                 <li><SiJavascript size={30}color="yellow"/><span className='lang-name'> JavaScript</span> </li>
                 <li><DiRuby size={30}color="red"/><span className='lang-name'>Ruby</span> </li>
                 <li><SiRubyonrails color='red' size={60}/><span className='lang-name'>Ruby on Rails</span> </li>
-              </ul>
-              <ul className='languages' style={{ marginTop: '-15px' }}>
                 <li><AiFillGithub color='black' size={30}/><span className='lang-name'>Github</span> </li>
                 <li><SiSqlite color='blue' size={30}/><span className='lang-name'>Sqlite3</span> </li>
               </ul>
